8fb39789060e9af8a8b12fd75fa5f51205767f5d
cseifert7817 <seifertc7817@uhcl.edu>
2025-11-24 19:21:52 +0000

    css reorg and edits

--- wolfsmiths_lounge/fp_wolfsmiths.js
+++ wolfsmiths_lounge/fp_wolfsmiths.js
@@ -12,7 +12,7 @@
              Oneida Gongora,
              Erick Mendoza
     Date started: 2025-11-03
-    Date finished: 2025-__-__ 
+    Date finished: 2025-11-24 
 
     Filename: fp_wolfsmiths.js
 
@@ -39,3 +39,4 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
